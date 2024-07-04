52dde91e8be7a9949bd53b8b9a0b8c9098e0c14d
Nikolay Dimitrov <n.zhdimitrov@gmail.com>
2024-07-04 18:02:17 +0300

    framer motion pkg

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,16 @@
-const Home = () => {
+import { SelectedPage } from "../utils/types.ts";
+import { motion } from "framer-motion";
+
+type Props = {
+    setSelectedPage: (value: SelectedPage) => void;
+};
+
+const Home = ({setSelectedPage}: Props) => {
 
     return <section id="home" className="gap-16 py-10 md:h-full md:pb-0">
+        <motion.div onViewportEnter={() => setSelectedPage(SelectedPage.Home)}>
 
+        </motion.div>
     </section>
 }
 
