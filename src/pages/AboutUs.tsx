125f91f3930cbe4d2358f51f54928ef6330bb83e
Nikolay Dimitrov <n.zhdimitrov@gmail.com>
2024-07-10 20:42:26 +0300

    .

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -5,13 +5,25 @@ import AboutImgSecurity from "../assets/About-Image-1.svg";
 import AboutImgFast from "../assets/About-Image-2.svg";
 import AboutImgMerchantTwo from "../assets/About-Image-3.svg";
 import AboutImgMerchant from "../assets/About-Image-4.svg";
+import AboutBackground from "../assets/About-Background.png";
+import PhotoOne from "../assets/Photo.jpg";
+import PhotoTwo from "../assets/Photo-1.jpg";
+import PhotoThree from "../assets/Photo-2.jpg";
+import Star from "../assets/Star.svg";
+import Award from "../assets/Award.svg";
 
 type Props = {
     setSelectedPage: (value: SelectedPage) => void;
 };
 
+const StarRating = ({ count = 5 }) => (
+    <div className="flex space-x-2">
+        {Array(count).fill().map((_, i) => <img key={i} alt="star" src={Star} />)}
+    </div>
+);
+
 const AboutUs = ({setSelectedPage}: Props) => {
-    return <section id="featured" className="mx-auto w-5/6 py-16">
+    return <section id="aboutus" className="mx-auto w-5/6 py-16">
         <motion.div
             onViewportEnter={() => setSelectedPage(SelectedPage.AboutUs)}
         >
@@ -86,6 +98,78 @@ const AboutUs = ({setSelectedPage}: Props) => {
                 </div>
             </div>
         </motion.div>
+        <div className="mt-20">
+            <img alt="About-page-background-bottom" src={AboutBackground} className="absolute w-10/12" />
+            <div className="absolute p-24 w-10/12">
+                <h3 className="text-center font-Jost font-semibold text-4xl text-white">
+                    "Say goodbye to clutter and
+                    <h3 className="text-primary">bounce rates."</h3>
+                </h3>
+                <div className="mt-12 w-3/4 mx-auto">
+                    <div className="border border-white bg-white rounded-2xl p-12">
+                        <div className="flex items-center justify-between">
+                            <div className="flex items-center gap-4">
+                                <img alt="team-member-photo" src={PhotoOne} className="rounded-full size-1/3"/>
+                                <div>
+                                    <h5 className="font-Jost font-medium text-primary text-xl">Nattasha</h5>
+                                    <h6 className="font-OpenSans text-secondary text-xs">Designer</h6>
+                                </div>
+                            </div>
+                            <div><StarRating /></div>
+                        </div>
+                        <div className="border bg-gray-line mx-auto my-6"></div>
+                        <p className="font-OpenSans text-content">
+                            “Our team was able to teach themselves primchat in a day. It's like
+                            using a shared email inboxust way more robust  looking.” Primchat
+                            was the modern what we were looking.
+                        </p>
+                    </div>
+                </div>
+                <div className="mt-12 w-3/4 mx-auto">
+                    <div className="border border-white bg-white rounded-2xl p-12">
+                        <div className="flex items-center justify-between">
+                            <div className="flex items-center gap-4">
+                                <img alt="team-member-photo" src={PhotoTwo} className="rounded-full size-1/3"/>
+                                <div>
+                                    <h5 className="font-Jost font-medium text-primary text-xl">Joe Cook</h5>
+                                    <h6 className="font-OpenSans text-secondary text-xs">Developer</h6>
+                                </div>
+                            </div>
+                            <div><StarRating /></div>
+                        </div>
+                        <div className="border bg-gray-line mx-auto my-6"></div>
+                        <p className="font-OpenSans text-content">
+                            “Our team was able to teach themselves primchat in a day. It's like
+                            using a shared email inboxust way more robust  looking.” Primchat
+                            was the modern what we were looking.
+                        </p>
+                    </div>
+                </div>
+                <div className="mt-12 w-3/4 mx-auto">
+                    <div className="border border-white bg-white rounded-2xl p-12">
+                        <div className="flex items-center justify-between">
+                            <div className="flex items-center gap-4">
+                                <img alt="team-member-photo" src={PhotoThree} className="rounded-full size-1/4"/>
+                                <div>
+                                    <h5 className="font-Jost font-medium text-primary text-xl">Jessica Jobs</h5>
+                                    <h6 className="font-OpenSans text-secondary text-xs">Product Designer at Uber.INC</h6>
+                                </div>
+                            </div>
+                            <div><StarRating /></div>
+                        </div>
+                        <div className="border bg-gray-line mx-auto my-6"></div>
+                        <p className="font-OpenSans text-content">
+                            “Our team was able to teach themselves primchat in a day. It's like
+                            using a shared email inboxust way more robust  looking.” Primchat
+                            was the modern what we were looking.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="pt-20 flex">
+            <div className="rounded border border-gray-line bg-gray-line bg-opacity-75 mt-10"> asd</div>
+        </div>
     </section>
 }
 
